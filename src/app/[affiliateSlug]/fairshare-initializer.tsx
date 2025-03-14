0818105570c02fff228ae54418d9f4094542b091
Subas poudel <poudel.subas089@gmail.com>
2025-03-14 17:47:55 +0545

    fairshare usuage

--- src/app/[affiliateSlug]/fairshare-initializer.tsx
+++ src/app/[affiliateSlug]/fairshare-initializer.tsx
@@ -1,18 +1,23 @@
 "use client";
 import { env } from "@/../env";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import FluidSDK from "@fluid-commerce/fairshare";
+import { Widgets } from "@fluid-commerce/widgets";
 
 export default function FairshareInitializer() {
-  useEffect(() => {
-    import('@fluid-commerce/fairshare').then(({ FluidSDK }) => {
-      FluidSDK.initialize({
-        fluid_shop: env.NEXT_PUBLIC_FLUID_BASE_URL,
-        apiUrlHost: env.NEXT_PUBLIC_FLUID_BASE_URL,
-        country: "us",
-      }).then(() => console.log("Fairshare initialized"));
-    })
-    .catch(err => console.error('Error importing FluidSDK:', err));
-  }, []);
+    const [isInitialized, setIsInitialized] = useState(false);
+    useEffect(() => {
+        FluidSDK.initialize({
+            fluid_shop: env.NEXT_PUBLIC_FLUID_SHOP_ID,
+            apiUrlHost: env.NEXT_PUBLIC_FLUID_BASE_URL,
+            country: "us",
+        }).then(() => {
+            setIsInitialized(true);
+            console.log("Fairshare initialized")
+        });
+    }, []);
 
-  return null;
+    return isInitialized ? (
+        <Widgets />
+    ) : null;
 }
